6fc9809cec30a99c675368f99cc82f4b30912c69
Link <Nerevarishe@users.noreply.github.com>
2020-07-06 14:21:33 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,3 +11,15 @@ class BaseDocument(Document):
     
     date_created = fl.DateTimeField(default=datetime.utcnow)
     date_edited = fl.DateTimeField(default=datetime.utcnow)      
+
+    
+class User(BaseDocument):
+    username = fl.StringField(max_length=30)
+    email = fl.EmailField
+    
+    
+class TaskCard(BaseDocument):
+    task_gtdue_date = fl.DateTimeField(default=datetime.utcnow)
+    task_type = fl.StringField(max_length=100, choices=['Reminder', 'Call', 'Event'])
+    task_status = fl.StringField(max_length=100, choices=['Active', 'Completed', 'Ended'])
+    task_body = fl.StringField(max_length=100)
