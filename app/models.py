b2518c5700ab091a0f4b8bc122d57fcd7a763b8a
Link <Nerevarishe@users.noreply.github.com>
2020-07-06 09:42:05 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
+from datetime import datetime
+from flask_mongoengine import Document
+from mongoengine import fields as fl
 
+
+class BaseDocument(Document):
+    
+    meta = {
+        'abstract': True
+    }
+    
+    date_created = fl.DateTimeField(default=datetime.utcnow)
+    date_edited = fl.DateTimeField(default=datetime.utcnow)      
