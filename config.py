e23ac683bf1b7a02091447e634378f5abfc50824
Nerevarishe <JIyHaTuK@gmail.com>
2020-08-09 14:08:17 +0300

    changing configuration

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
 
     # Flask-MongoEngine Settings
-    MONGODB_DB = os.environ.get('MONGODB_DB') or 'test_sass'
+    MONGODB_DB = os.environ.get('MONGODB_DB') or 'test'
     MONGODB_HOST = os.environ.get('MONGODB_HOST') or '10.0.0.1'
     MONGODB_PORT = os.environ.get('MONGODB_PORT') or 27017
     MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME') or ''
